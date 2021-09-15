ed6b8f2cd80c042e0595554c579d01a2396b9254
barton <barton.li@hr-mp.com>
2021-09-15 10:08:28 +0800

    导入优化

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
-echo date_default_timezone_get();
-echo strtotime('2021-08-01 09:51:00');
-exit();
 require __DIR__ . '/vendor/autoload.php';
 
 use think\facade\Db;
@@ -57,11 +54,45 @@ function loadAlipay()
     $sheet = $spreadsheet->getActiveSheet();
     for ($i=3; $i<=$sheet->getHighestRow() - 21; $i++) {
         $arr = explode(',', $sheet->getCell('A'.$i)->getValue());
+
+        // 格式化
         $type_str = trim($arr[0]);
         $type = $type_str == '收入' ? 1 : ($type_str == '支出' ? 2 : 0);
-
         $trans_time_str = trim($arr[10]);
         $trans_time = strtotime($trans_time_str);
+
+        // 过滤
+        if (in_array(trim($arr[7]), ['投资理财', '信用借还'])) {
+            continue;
+        }
+
+        // 标签
+        switch (trim($arr[7])) {
+            case '交通出行':
+                $tag = 1; // 交通
+                break;
+            case '餐饮美食':
+                $tag = 2; // 餐饮
+                break;
+            case '服饰装扮':
+            case '美容美发':
+                $tag = 3; // 穿搭美容
+                break;
+            case '日用百货':
+                $tag = 4; // 生活日用
+                break;
+            case '充值缴费':
+                $tag = 5; // 生活服务
+                break;
+            case '文化休闲':
+                $tag = 6; // 休闲娱乐
+                break;
+            default:
+                $tag = null;
+        }
+        if (trim($arr[1]) == '妈妈驿站') {
+            $tag = 0;
+        }
 
         $data = [
             'account' => $config['alipay']['account'],
@@ -73,7 +104,8 @@ function loadAlipay()
             'amount' => trim($arr[5]) * ($type == 2 ? -1 : 1),
             'trans_no' => trim($arr[8], " \t\n\r\0\x0B\""),
             'merchant_order_no' => trim($arr[9], " \t\n\r\0\x0B\""),
-            'trans_time' => $trans_time
+            'trans_time' => $trans_time,
+            'tag' => $tag,
         ];
         try {
             Db::name('personbill')->insert($data);
@@ -100,9 +132,11 @@ function loadWeChat()
         $arr = explode(',', $sheet->getCell('A'.$i)->getValue());
         $type_str = trim($arr[4]);
         $type = $type_str == '收入' ? 1 : ($type_str == '支出' ? 2 : 0);
-
         $trans_time_str = trim($arr[0]);
         $trans_time = strtotime($trans_time_str);
+
+        // 标签
+        $tag = null;
 
         $data = [
             'account' => $config['wechat']['account'],
@@ -114,7 +148,8 @@ function loadWeChat()
             'amount' => trim($arr[5], " \t\n\r\0\x0B¥") * ($type == 2 ? -1 : 1),
             'trans_no' => trim($arr[8]),
             'merchant_order_no' => trim($arr[9]),
-            'trans_time' => $trans_time
+            'trans_time' => $trans_time,
+            'tag' => $tag,
         ];
         try {
             Db::name('personbill')->insert($data);
@@ -141,7 +176,7 @@ function loadCCB()
         $trans_time_str = trim($sheet->getCell('B'.$i)->getValue()) . ' ' . trim($sheet->getCell('C'.$i)->getValue());
         $trans_time = strtotime($trans_time_str);
 
-        Db::name('personbill')->insert([
+        $data = [
             'account' => $config['ccb']['account'],
             'account_type_name' => '建设银行',
             'trans_type_name' => '银行卡',
@@ -149,8 +184,18 @@ function loadCCB()
             'type' => $type,
             'description' => trim($sheet->getCell('H'.$i)->getValue()),
             'amount' => $type == 2 ? -1 * $sheet->getCell('D'.$i)->getValue() : $sheet->getCell('E'.$i)->getValue(),
+            'trans_no' => trim($sheet->getCell('A'.$i)->getValue()),
             'trans_time' => $trans_time
-        ]);
+        ];
+        try {
+            Db::name('personbill')->insert($data);
+        } catch (\think\db\exception\PDOException $e) {
+            if ($e->getCode() == 10501) {
+                continue;
+            }
+
+            throw $e;
+        }
     }
 }
 
@@ -158,15 +203,15 @@ function main()
 {
     global $config;
 
-//    if (file_exists($config['ccb']['filename'])) {
+//    if ($config['ccb']['filename'] && file_exists($config['ccb']['filename'])) {
 //        loadCCB();
 //    }
-    if (file_exists($config['alipay']['filename'])) {
+    if ($config['alipay']['filename'] && file_exists($config['alipay']['filename'])) {
         loadAliPay();
     }
-    if (file_exists($config['wechat']['filename'])) {
-        loadWeChat();
-    }
+//    if ($config['wechat']['filename'] && file_exists($config['wechat']['filename'])) {
+//        loadWeChat();
+//    }
 }
 
 main();
